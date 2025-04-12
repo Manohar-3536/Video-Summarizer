05b76e6a5941618c2691690637954af1839f7d7a
Manohar <gvsmanohar4535@gmail.com>
2025-04-12 15:23:58 +0530

    Updated index.py with correct application setup for Render

--- back-end/api/summarize-youtube/index.py
+++ back-end/api/summarize-youtube/index.py
@@ -46,6 +46,8 @@ def get_summary(transcript):
         summary += summary_text + ' '
     return summary.strip()
 
+application = app
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
